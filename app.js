2017ae1fc80db9e82c423581d8b098739a46d5ee
Luke Judd <lukewjudd@gmail.com>
2014-03-21 04:08:27 +0000

    try create server

--- app.js
+++ app.js
@@ -57,7 +57,11 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-var port = process.env.PORT || 80;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+//var port = process.env.PORT || 80;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
+
+http.createServer(app).listen(3000, function(){
+  console.log("Express server listening on port 3000");
 });
